08ab0e43691f0534baf90455863d0406ef2c9b74
sanabinadaf <71943182+ishannadaf@users.noreply.github.com>
2026-01-22 08:06:11 +0530

    Graph

---
+++ Trees/AVL_Trees.py
@@ -0,0 +1,9 @@
+"""
+Docstring for Trees.AVL_Trees
+
+An AVL tree is a self-balancing binary search tree where the difference between heights of left 
+and right subtrees cannot be more than one for all nodes.
+
+
+
+"""
